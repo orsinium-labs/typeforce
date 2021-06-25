82ad723b1f76b87ccb735cf2b92e2274cb7aa810
gram <mail@orsinium.dev>
2021-06-25 09:19:49 +0200

    install missed  stubs

--- typeforce/_core.py
+++ typeforce/_core.py
@@ -1,6 +1,7 @@
 import subprocess
 import typing
 from pathlib import Path
+from ._constants import STUBS
 
 
 RED = '\033[91m'
@@ -34,6 +35,11 @@ class Module(typing.NamedTuple):
         return (self.path / MARKER).exists()
 
     @property
+    def stubbed(self) -> bool:
+        new_name = self.path.name + '-stubs'
+        return (self.path.parent / new_name).exists()
+
+    @property
     def annotated(self) -> bool:
         script = SCRIPT.format(name=self.path.name)
         result = subprocess.run([self.exe, '-c', script])
@@ -43,6 +49,18 @@ class Module(typing.NamedTuple):
     def process(self) -> str:
         if self.patched:
             return GREEN + 'already annotated' + END
+        if self.stubbed:
+            return GREEN + 'already has stubs' + END
+
+        stub = STUBS.get(self.path.name)
+        if stub:
+            if not self.dry:
+                cmd = [self.exe, '-m', 'pip', 'install', stub]
+                result = subprocess.run(cmd, stdout=subprocess.DEVNULL)
+                if result.returncode != 0:
+                    return RED + 'cannot install stubs' + END
+            return YELLOW + 'stubs installed' + END
+
         if self.annotated:
             if not self.dry:
                 (self.path / MARKER).write_bytes(b'')
